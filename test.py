8ebe9cf44378bdfec2b803625168a1c26ce433b2
BobVitorBob <vitordecastrosilva@gmail.com>
2022-11-04 11:41:01 -0300

    Adicionando teste em dataset reail

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from Separators.SeparatorPageHinkley import SeparatorPageHinkley
 from plot import plot
 import math
+import pandas as pd
 import random
 
 if __name__ == '__main__':
 	sep = SeparatorPageHinkley()
-	series = [(math.sin(x/100)) for x in range(1000)] + [(math.sin(x/10)) for x in range(1000)]
+	series = pd.read_csv('../../../Dados/por estacao/23025122/export_automaticas_23025122_umidrelmed2m.csv', nrows=2000)['umidrelmed2m']
+	# series = [(math.sin(x/100)) for x in range(1000)] + [(math.sin(x/10)) for x in range(1000)]
 	plot(series)
-	cuts = sep.create_splits(series)
-	print(cuts)
-	plot(series, divisions=cuts)
+	cuts, threshold = sep.create_splits(series)
+	print(cuts, threshold)
+	plot(series, divisions=cuts, title=threshold)
