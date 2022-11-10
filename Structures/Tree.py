3e74c45fc97ec01cf5e374da9decbc4f2941eb3d
BobVitorBob <vitordecastrosilva@gmail.com>
2022-11-10 17:16:23 -0300

    Adicionando função to_list, get_leaves e __len__

--- Structures/Tree.py
+++ Structures/Tree.py
@@ -2,51 +2,39 @@ class TreeNode:
   pass
 class TreeNode:
 
-	def __init__(self, content):
-		self._left = None
-		self._right = None
-		self.tree = None
-		self.cont = content
+  def __init__(self, content):
+    self.left = None
+    self.right = None
+    self.cont = content
 
-	@property
-	def left():
-		return self._left
-
-	@left.setter
-	def left(node: TreeNode):
-		if self._left == None:
-			self.tree._grow()
-
-		self._left = node
-		self._left.tree = self.tree
-  
-	@property
-	def right():
-		return self._right
-
-	@right.setter
-	def right(node: TreeNode):
-		if self._right == None:
-			self.tree._grow()
-
-		self._right = node
-		self._right.tree = self.tree
-
-	
 class Tree:
 
-	def __init__(self):
-		self.len = 0
-	
-	@property
-	def root():
-		return self._root
+  def __init__(self):
+    self.root = None
+  
+  def to_list(self):
+    return Tree._sub_node_to_list(self.root)
 
-	@root.setter
-	def root(node: TreeNode):
-		self.len += 1
-		self._root = node
-		self._root._set_tree(self)
-	
-	def _grow():
-		self.len += 1
+  def get_leaves(self):
+    return Tree._get_leaves(self.root)
+      
+  @staticmethod
+  def _get_leaves(node: TreeNode):
+    if node.left is None and node.right is None:
+      return [node.cont]
+    return [*Tree._get_leaves(node.left), *Tree._get_leaves(node.right)]
+
+  @staticmethod
+  def _sub_node_to_list(node: TreeNode):
+    if node is None:
+      return []
+    return [*Tree._sub_node_to_list(node.left), node.cont, *Tree._sub_node_to_list(node.right)]
+
+  def __len__(self):
+    return Tree._count_nodes(self.root)
+    
+  @staticmethod
+  def _count_nodes(node: TreeNode):
+    if node is None:
+      return 0
+    return 1 + Tree._count_nodes(node.left) + Tree._count_nodes(node.right)
