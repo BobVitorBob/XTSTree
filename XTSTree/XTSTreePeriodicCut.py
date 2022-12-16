ad4e58a51617a213c3223542ddad50638dad16f3
BobVitorBob <vitordecastrosilva@gmail.com>
2022-12-16 11:46:26 -0300

    Adding periodic cut class

---
+++ XTSTree/XTSTreePeriodicCut.py
@@ -0,0 +1,11 @@
+from XTSTree.XTSTree import XTSTree
+from collections.abc import Iterable
+
+class XTSTreePeriodicCut(XTSTree):
+  
+  def __init__(self, stop_condition: str='depth', stop_val=2, max_iter=1000, min_dist=30):
+    super().__init__(stop_condition=stop_condition, stop_val=stop_val, max_iter=max_iter, min_dist=min_dist)
+
+  def _find_cut(self, series: Iterable, params: dict):
+    cut = int(len(series)/2)
+    return cut, params
