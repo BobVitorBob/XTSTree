10784b649d269172480a91c57ba1319948fcf701
barbon <sbarbonjr@gmail.com>
2022-12-30 12:44:53 +0100

    Adding parameter from terminal.

--- test/experiments_symbreg.py
+++ test/experiments_symbreg.py
@@ -130,6 +130,8 @@ def get_regressor(criteria, file, cut, iterations, path):
 #    param_niterations = sys.argv[0]
 
 
+param_dataset = sys.argv[0]
+
 #remover depois
 param_path = "test/"
 param_niterations = 50
@@ -141,7 +143,7 @@ max_std = 15
 std = min_std
 adf = 0.05
 
-dir_path = param_path+'datasets/umidrelmed2m/60dias/'
+dir_path = param_path+'datasets/umidrelmed2m/'+param_dataset+'/'
 list_files = listing_all_files(dir_path)
 
 list_criteria = ["best", "accuracy"]
@@ -258,4 +260,4 @@ df_experiment_log = pd.DataFrame(experiment_log)
 df_experiment_log.columns = ["File", "XTSTree", "Cuts", "Time", "Criteria", "NumIterations", "time raw", "time leaves",
                              "MAE", "MSE", "RMSE", "MAPE",
                              "Mean_Leaf_MAE", "Mean_Leaf_MSE", "Mean_Leaf_RMSE", "Mean_Leaf_MAPE"]
-df_experiment_log.to_csv(param_path+"experiment_log_60.csv")
+df_experiment_log.to_csv(param_path+"experiment_log_"+param_dataset+".csv")
